96bfd21af347c35fb9c6c03667fa13d7aab6aa6f
Raj <joshiraj008@gmail.com>
2021-03-06 11:05:31 +0100

    Added a auth-timeout.php to expire logged-in session after 20 minutes of in-activity.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,11 +51,11 @@
                                     {{ __('Anmeldung') }}
                                 </button>
 
-                                <!--  @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a> -->
-                                @endif
+{{--                                  @if (Route::has('password.request'))--}}
+{{--                                    <a class="btn btn-link" href="{{ route('password.request') }}">--}}
+{{--                                        {{ __('Forgot Your Password?') }}--}}
+{{--                                    </a>--}}
+{{--                                @endif--}}
                             </div>
                         </div>
                     </form>
