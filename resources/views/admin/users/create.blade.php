20e95413efba96a52decb5f387c7f399e4ef3b38
Raj Joshi <joshiraj008@gmail.com>
2021-10-21 22:40:49 +0200

    Added a checkbox to view password while creating user in Admin Panel.

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -40,6 +40,7 @@
                             <input name="password" type="password"
                                    class="form-control @error('password') is-invalid @enderror"
                                    id="password">
+                            <input name="checkbox" type="checkbox" onclick="myFunction()"> Kennwort anzeigen
                             @error('password')
                             <span class="invalid-feedback" role="alert">{{ $message }}</span>
                             @enderror
@@ -77,4 +78,14 @@
             </div>
         </div>
     </div>
+    <script>
+        function myFunction() {
+            let x = document.getElementById("password");
+            if (x.type === "password") {
+                x.type = "text";
+            } else {
+                x.type = "password";
+            }
+        }
+    </script>
 @endsection
